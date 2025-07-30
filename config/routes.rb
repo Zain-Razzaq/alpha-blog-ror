994845fa9369c3cdf4b26751d92c59ea572c3b8d
Zain-Razzaq <zainrazzaq2003@gmail.com>
2025-07-30 22:02:20 +0500

    Updated the destroy functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'register', to: 'users#new', as: 'register'
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 end
