ec47ff6911898619a875c4eb59196a9972befa40
Zain-Razzaq <zainrazzaq2003@gmail.com>
2025-07-31 12:26:06 +0500

    Added helper functions to check current user and navbar updated

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+const toggle = document.getElementById("menuToggle");
+const navbar = document.getElementById("navbar");
+
+toggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
